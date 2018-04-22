2dc901a561df2e8df9a9baf09a14e5dd713a01d3
cwadsworth33 <cwadsrun33@gmail.com>
2018-04-22 17:55:20 -0400

    Have functional sidebar. Added functionality to sidebar.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Chess Opening Trainer
 This app will allow users to enter chess openings and practice memorizing them.
+Built using chessboard.js and the Materialize css framework.
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-class Move {
-  constructor(move, title){
-    this.move = move;
+class Opening {
+  constructor(fen, title){
+    this.fen = fen;
     this.title = title;
     this.children = [];
   }
@@ -10,12 +10,56 @@ class Move {
   }
 }
 
+var globals = {
+  openings: []
+};
+
 $(function(){
   $('.sidenav').sidenav();
   var board1 = ChessBoard('board1',
   {position: 'start',
   draggable: true
   });
-  board1.move('e2-e4', 'c7-c5');
 
+  $("#sidenav-my_openings").click(function(){
+    initializeMyOpenings();
+    $('.sidenav').sidenav('close');
+  });
+
+  $("#sidenav-new_openings").click(function(){
+    initializeNewOpening();
+    $('.sidenav').sidenav('close');
+  });
+
+  $("#add-opening").click(function(){
+    var name = $("#opening_name").val();
+    var opening = new Opening("", name);
+    globals.openings.push(opening);
+    $("#opening_name").val('');
+    initializeMyOpenings();
+  });
 });
+
+
+var initializeMyOpenings = function(){
+  hideAll();
+  $("#board-and-menu").removeClass("hide");
+  $("#display-menu-collection").html('<div class="collection-header">' +
+    '<h4 id="collection-title">My Openings</h4>' +
+  '</div>');
+  for(var i=0; i<globals.openings.length; i++){
+    var c = $("<a class='collection-item blue-text' href='#'>" + globals.openings[i].title + "</a>");
+    $("#display-menu-collection").append(c);
+  }
+};
+
+var initializeNewOpening = function(){
+  hideAll();
+  $("#new-opening-form-container").removeClass("hide");
+};
+
+var hideAll = function(){
+  $("#board-and-menu").addClass("hide");
+  $("#new-opening-form-container").addClass("hide");
+  $("#home-page").addClass("hide");
+}
